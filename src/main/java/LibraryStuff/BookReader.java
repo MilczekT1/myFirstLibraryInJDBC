e7e77672581a67c493a607e531d25433e2f27b83
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-05-12 14:22:39 +0200

    implement LibraryItem interface and add private id

--- src/main/java/LibraryStuff/BookReader.java
+++ src/main/java/LibraryStuff/BookReader.java
@@ -3,11 +3,18 @@ package LibraryStuff;
 import lombok.Data;
 
 @Data
-public class BookReader {
+public class BookReader implements LibraryItem {
+    private Integer id;
     private String name;
     private String surname;
     
     public BookReader(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    
+    public BookReader(String name, String surname, Integer id) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
     }
