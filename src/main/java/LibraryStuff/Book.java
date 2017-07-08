ad929cd9b3c155224947bd5a29a9db9002fc86d3
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-07-08 10:47:29 +0200

    Book class fixed and tested

--- src/main/java/LibraryStuff/Book.java
+++ src/main/java/LibraryStuff/Book.java
@@ -11,13 +11,23 @@ class Book implements LibraryItem {
     
     public Book(){
     }
-    public Book(String title, int pages) {
-        this.title = title;
-        this.pages = pages;
+    public Book(String title, int pages) throws IllegalArgumentException {
+        if (title != null && pages >0) {
+            this.title = title;
+            this.pages = pages;
+        }
+        else {
+            throw new IllegalArgumentException("Incorrect input in 2 arg constructor");
+        }
     }
-    public Book(String title, int pages, int id) {
-        this.title = title;
-        this.pages = pages;
-        this.id = id;
+    public Book(String title, int pages, int id) throws IllegalArgumentException {
+        if (title != null && pages > 0 && id > 0){
+            this.title = title;
+            this.pages = pages;
+            this.id = id;
+        }
+        else{
+            throw new IllegalArgumentException("Incorrect input in 3 arg constructor");
+        }
     }
 }
---
+++ src/test/java/LibraryStuff/TestBook.java
@@ -0,0 +1,6 @@
+package LibraryStuff;
+
+public class TestBook {
+    
+    
+}
