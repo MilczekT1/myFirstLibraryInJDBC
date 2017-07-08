2dab8c7e81e71a5267ee0d630fd2a7116b3e7c4c
Konrad Boniecki <konrad_boniecki@hotmail.com>
2017-07-08 15:37:47 +0200

    fixed constructors

--- src/main/java/LibraryStuff/Rent.java
+++ src/main/java/LibraryStuff/Rent.java
@@ -9,14 +9,24 @@ class Rent implements LibraryItem {
     private Integer bookID;
     private Integer readerID;
     
-    public Rent(Integer bookID, Integer readerID) {
-        this.bookID = bookID;
-        this.readerID = readerID;
+    public Rent(Integer bookID, Integer readerID) throws IllegalArgumentException {
+        if (bookID != null && readerID != null && bookID > 0 && readerID > 0){
+            this.bookID = bookID;
+            this.readerID = readerID;
+        } else {
+            throw new IllegalArgumentException("Incorrect input in 2 arg constructor");
+        }
     }
     
     public Rent(Integer bookID, Integer readerID, Integer rentID) {
-        this.rentID = rentID;
-        this.bookID = bookID;
-        this.readerID = readerID;
+        if (bookID != null &&  bookID > 0 &&
+            readerID != null && readerID > 0 &&
+            rentID != null && rentID > 0){
+            this.rentID = rentID;
+            this.bookID = bookID;
+            this.readerID = readerID;
+        } else {
+            throw new IllegalArgumentException("Incorrect input in 3 arg constructor");
+        }
     }
 }
